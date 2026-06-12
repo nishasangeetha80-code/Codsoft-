cde96f3b3cda4d3980c2c49e8103b299c52d12af
nishasangeetha80-code <nishasangeetha80@gmail.com>
2026-06-12 16:13:20 +0530

    Fix calculator input and operator logic
    Fix input handling and operator checks in Calculator.py.

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,14 @@
-
+print("Simple Calculator")
+a=int(input())
+b=int(int(input())
+op="Enter a operator(+,-,/,*):"
+if op==+:
+  print("addition :",a+b)
+elif op==-:
+  print(" Subtraction:",a-b)
+elif op== *:
+  print(" Multiplication:",a*b)
+elif op==/:
+  print("Division:",a/b)
+else:
+  print("Invalid Operator")
