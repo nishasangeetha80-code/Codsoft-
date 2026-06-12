1aa231696d6b955e091aebe1d6c59d750271b587
nishasangeetha80-code <nishasangeetha80@gmail.com>
2026-06-12 16:34:08 +0530

    Refactor input prompts and output formatting

--- Calculator.py
+++ Calculator.py
@@ -1,16 +1,16 @@
 print("Simple Calculator")
-a=int(input())
-b=int(input())
-op=input("Enter a operator(+,-,/,*):")
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+op=input("Enter an operator(+,-,/,*):")
 if op=="+":
-  print("addition :",a+b)
+  print("addition:",a+b)
 elif op=="-":
-  print(" Subtraction:",a-b)
-elif op== "*":
-  print(" Multiplication:",a*b)
+  print("Subtraction:",a-b)
+elif op=="*":
+  print("Multiplication:",a*b)
 elif op=="/":
   if b==0:
-    print("Input proper number")
+    print("Second number should not be zero")
   else:
     print("Division:",a/b)
 else:
