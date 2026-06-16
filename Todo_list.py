6f7b3a9a33e82639ee33bbf1e4277cf795d05b3e
nishasangeetha80-code <nishasangeetha80@gmail.com>
2026-06-16 16:27:22 +0530

    Improve input prompts in Todo_list.py

--- Todo_list.py
+++ Todo_list.py
@@ -5,17 +5,17 @@ while True:
   print("3.update Task")
   print("4.Delete Task")
   print('5.Exit')
-  ch=int(input())
+  ch=int(input("Enter a choice:"))
   if ch==1:
     Task= input(" Enter a task:")
     Tasks.append(Task)
   elif ch==2:
     print(Tasks)
   elif ch==3:
-    n=int(input("Enter a numbers:")
+    n=int(input("Enter a numbers:"))
     Tasks[n-1]=input("Enter a number:")
   elif ch==4:
-    n=int(input("Enter a number:")
+    n=int(input("Enter a number:"))
     Tasks.pop(n-1)
   elif ch==5:
     break
