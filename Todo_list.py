c420e94987b25ca6cdccf8221625f278d31b44f0
nishasangeetha80-code <nishasangeetha80@gmail.com>
2026-06-16 16:14:06 +0530

    Add basic task management functionality
    Implemented a simple command-line task manager allowing users to add, view, update, and delete tasks.

--- Todo_list.py
+++ Todo_list.py
@@ -1,1 +1,25 @@
+Tasks=[]
+while True:
+  print("\n 1.Add Task")
+  print('2.View Task')
+  print("3.update Task")
+  print("4.Delete Task")
+  print('5.Exit')
+  ch=int(input())
+  if ch==1:
+    Task= input(" Enter a task:")
+    Tasks.append(Task)
+  elif ch==2:
+    print(Tasks)
+  elif ch==3:
+    n=int(input("Enter a numbers:")
+    Tasks[n-1]=input("Enter a number:")
+  elif ch==4:
+    n=int(input("Enter a number:")
+    Tasks.pop(n-1)
+  elif ch==5:
+    break
+  else:
+    print("Invalid choice")
+  
 
