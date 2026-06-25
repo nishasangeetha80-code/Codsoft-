6c376c6ab8f74ce61e037e1e1e32267284aa152e
nishasangeetha80-code <nishasangeetha80@gmail.com>
2026-06-25 11:10:12 +0530

    Update Rock_paper_scissors.py

--- Rock_paper_scissors.py
+++ Rock_paper_scissors.py
@@ -3,4 +3,9 @@ choices=['stone','paper','scissors']
 user=input("Enter a stone, paper or scissors:").lower()
 computer=random.choice(choices)
 if computer==user:
+print('It is a Tie')
+elif (user= and computer=) or (user= and computer=) or (user= and computer=):
+print('You win')
+else:
+print('computer wins')
 
